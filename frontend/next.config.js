e47b3f78d7b8d49a526a976d16eaed3e575237e6
Ashutosh Sahoo <sahooashutosh2022@gmail.com>
2026-05-13 22:31:08 +0530

    Fix: Remove invalid next.config upgrade to ESLint 9 flat config

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,10 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  eslint: {
-    // Lint is handled separately; don't fail Vercel build on ESLint config incompatibilities
-    ignoreDuringBuilds: true,
-  },
 };
 
 module.exports = nextConfig;
